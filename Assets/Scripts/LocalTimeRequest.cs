145937a554452f47e09d931248629d2a24087bbf
Mohammed Arshad <marsha6@uic.edu>
2022-09-15 18:47:48 -0500

    Vuforia Packages Added w/ LFS

---
+++ Assets/Scripts/LocalTimeRequest.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+
+public class LocalTimeRequest : MonoBehaviour
+{
+    string url = "http://worldtimeapi.org/api/timezone/Europe/Paris";
+    public GameObject timeTextObject;
+    void Start()
+    {
+        InvokeRepeating("GetDataFromWeb", 2f, 1f);
+    }
+
+    void GetDataFromWeb()
+    {
+        StartCoroutine(GetRequest(url)); // Starting CoRoutine
+    }
+
+    public class TimeClass
+    {
+        public string datetime;
+
+        public static TimeClass CreateFromJSON(string jsonString)
+        {   
+            return JsonUtility.FromJson<TimeClass>(jsonString);
+        }
+        public string GetStandardTime()
+        {
+            string timeSegment = this.datetime.Split('T')[1][0..5];
+            return timeSegment + "PM";
+        }
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+            string rawJson = webRequest.downloadHandler.text;
+            TimeClass newJson = TimeClass.CreateFromJSON(rawJson);
+            string timeString = newJson.GetStandardTime();
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    timeTextObject.GetComponent<TextMeshPro>().text = timeString;
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WeatherRequest.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+
+public class WeatherRequest : MonoBehaviour
+{
+    string url = "https://api.openweathermap.org/data/2.5/weather?lat=48.8534&lon=2.3488&appid=a93b0c11404d077c9140f5f9bdf16892";
+    public GameObject WeatherTextObject;
+    void Start()
+    {
+        InvokeRepeating("GetDataFromWeb", 2f, 1f);
+    }
+
+    void GetDataFromWeb()
+    {
+        StartCoroutine(GetRequest(url)); // Starting CoRoutine
+    }
+
+
+    
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+            string rawJson = webRequest.downloadHandler.text;
+            WeatherClass weatherInfo = JsonUtility.FromJson<WeatherClass>(rawJson);
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    WeatherTextObject.GetComponent<TextMeshPro>().text = (weatherInfo.main.temp - 273).ToString() + " C";
+                    Debug.Log(weatherInfo.main.temp);
+                    break;
+            }
+        }
+    }
+}
+
+[Serializable]
+public class WeatherClass
+{
+    public Coord coord;
+    public Weather weather;
+    public Main main;
+    public string @base;
+    public string visibility;
+    public Wind wind;
+    public Rain rain;
+    public Clouds clouds;
+    public string dt;
+    public Sys sys;
+    public string timezone;
+    public string id;
+    public string name;
+    public string code;
+}
+
+[Serializable]
+public class Coord
+{
+    public double lon;
+    public double lat;
+}
+
+[Serializable]
+public class Weather
+{
+    public int id;
+    public string main;
+    public string description;
+    public string icon;
+}
+
+[Serializable]
+public class Main
+{
+    public double temp;
+    public double feels_like;
+    public double temp_min;
+    public double temp_max;
+    public int pressure;
+    public int humidity;
+    public int sea_level;
+    public int grnd_level;
+}
+
+[Serializable]
+public class Wind
+{
+    public double speed;
+    public double deg;
+    public double gust;
+}
+
+[Serializable]
+public class Rain
+{
+    public double lh;
+}
+
+[Serializable]
+public class Clouds
+{
+    public int all;
+}
+
+[Serializable]
+public class Sys
+{
+    public int type;
+    public string id;
+    public string country;
+    public string sunrise;
+    public string sunset;
+}
